66cec981da272d1225c3a79c22c7e49c236aac0c
Simon Breiter <hello@simonbreiter.com>
2017-05-18 18:31:30 +0200

    Reformat code

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-'use strict';
-var express = require('express');
-var app = express();
-var colors = require('./colors.json');
+let express = require('express')
+let app = express()
+let colors = require('./colors.json')
 
-app.use('/public', express.static(__dirname + '/public'));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/public', express.static(__dirname + '/public'))
+app.use('/node_modules', express.static(__dirname + '/node_modules'))
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
-  res.render('index', {'colors': colors});
-});
+  res.render('index', {'colors': colors})
+})
 
 app.listen(3000, function () {
-  console.log('Listening on port 3000!');
-});
+  console.log('Listening on port 3000!')
+})
 
